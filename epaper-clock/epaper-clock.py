3b552a3982522d13cabab721e127346701bbebd2
root <root@jeremy-rasp-epap.localdomain>
2018-05-11 18:39:02 +0800

    Jeremy Network show

--- epaper-clock/epaper-clock.py
+++ epaper-clock/epaper-clock.py
@@ -71,18 +71,20 @@ TIMEFORMAT = "%H:%M"
 FONT = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
 
 class Fonts:
-    def __init__(self, timefont_size, datefont_size):
+    def __init__(self, timefont_size, datefont_size, datafont_size):
         self.timefont = ImageFont.truetype(FONT, timefont_size)
         self.datefont = ImageFont.truetype(FONT, datefont_size)
+        self.datafont = ImageFont.truetype(FONT, datafont_size)
 
 def main():
-    locale.setlocale(locale.LC_ALL, LOCALE)
+    #locale.setlocale(locale.LC_ALL, LOCALE)
+    locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
 
     epd = epd2in7b.EPD()
     epd.init()
     epd.set_rotate(epd2in7b.ROTATE_270)
 
-    fonts = Fonts(timefont_size = 75, datefont_size = 30)
+    fonts = Fonts(timefont_size = 60, datefont_size = 20, datafont_size = 20)
 
     read_button4_for_shutdown()
     clock_loop(epd, fonts)
@@ -103,9 +105,10 @@ def draw_clock_data(epd, fonts, datetime_now):
     frame_black = [0] * (epd.width * epd.height / 8)
     frame_red = [0] * (epd.width * epd.height / 8)
 
-    epd.draw_string_at(frame_black, 20, 20, timestring, fonts.timefont, COLORED)
-    epd.draw_string_at(frame_black, 20, 100, datestring, fonts.datefont, COLORED)
-#    epd.draw_string_at(frame_red, 50, 120, "e-paper.", font, COLORED)
+    epd.draw_string_at(frame_black, 20, 10, timestring, fonts.timefont, COLORED)
+    epd.draw_string_at(frame_red, 20, 70, datestring, fonts.datefont, COLORED)
+    epd.draw_string_at(frame_black, 20, 90, "Jeremy Wu", fonts.datafont, COLORED)
+    fn_ip_show(epd, fonts, frame_black, frame_red)
     epd.display_frame(frame_black, frame_red)
 
 def read_button4_for_shutdown():
@@ -113,6 +116,64 @@ def read_button4_for_shutdown():
     pin = 19  # 4th button in the 2.7 inch hat this pin according to the schematics.
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=shutdown_button_pressed, bouncetime=200)
+
+# management interface name
+mgmt_intf = 'eth0'
+mgmt_intf_wlan1 = 'wlan1'
+mgmt_intf_ip_addr = ['192', '168', '000', '001']
+mgmt_intf_prefix = 24
+
+def fn_intf_is_avail(intf):
+    import netifaces
+
+    intf_list = netifaces.interfaces()
+    return (intf in intf_list)
+
+def fn_intf_is_linkup(intf):
+    link_state = tuple(open('/sys/class/net/'+intf+'/operstate', 'r'))
+    return ('up' in link_state[0])
+
+def fn_ip_show(epd, fonts, frame_black, frame_red):
+    import socket
+    import netifaces
+    global mgmt_intf
+    global mgmt_intf_wlan1
+
+#    buttons = (
+#        rdas_lcd_plate.BTN_SEL, rdas_lcd_plate.BTN_UP, rdas_lcd_plate.BTN_DOWN, rdas_lcd_plate.BTN_LEFT, rdas_lcd_plate.BTN_RIGHT # return
+#    )
+
+    # show IP address and subnet mask settings
+    addr_info_wlan1 = False
+    if fn_intf_is_avail(mgmt_intf) is False:
+        epd.draw_string_at(frame_red, 20, 120, mgmt_intf+' not exist', fonts.datafont, COLORED)
+    elif fn_intf_is_linkup(mgmt_intf) is False:
+        epd.draw_string_at(frame_red, 20, 120, mgmt_intf+' link-down', fonts.datafont, COLORED)
+    else:
+        addr_info = netifaces.ifaddresses(mgmt_intf)
+        if netifaces.AF_INET in addr_info.keys():
+            ip_addr = addr_info[netifaces.AF_INET][0]['addr']
+            netmask = addr_info[netifaces.AF_INET][0]['netmask']
+            epd.draw_string_at(frame_black, 20, 120, 'Eth0 : ', fonts.datafont, COLORED)
+            epd.draw_string_at(frame_red, 100, 120, ip_addr, fonts.datafont, COLORED)
+        else:
+            epd.draw_string_at(frame_red, 20, 120, 'Eth0 ha no address.', fonts.datafont, COLORED)
+
+        if fn_intf_is_avail(mgmt_intf_wlan1) is False:
+            epd.draw_string_at(frame_red, 20, 135, mgmt_intf_wlan1+' not exist', fonts.datafont, COLORED)
+        elif fn_intf_is_linkup(mgmt_intf_wlan1) is False:
+            epd.draw_string_at(frame_red, 20, 135, mgmt_intf_wlan1+' link-down', fonts.datafont, COLORED)
+        else:
+            addr_info_wlan1 = netifaces.ifaddresses(mgmt_intf_wlan1)
+            if netifaces.AF_INET in addr_info_wlan1.keys():
+                ip_addr_wlan1 = addr_info_wlan1[netifaces.AF_INET][0]['addr']
+                epd.draw_string_at(frame_black, 20, 120, 'Wlan0 : ', fonts.datafont, COLORED)
+                epd.draw_string_at(frame_red, 100, 135, ip_addr_wlan1, fonts.datafont, COLORED)
+
+            else:
+                epd.draw_string_at(frame_red, 20, 135, 'Wlan0 has no address.', fonts.datafont, COLORED)
+
+
 
 def shutdown_button_pressed(pin):
     print("Button %d was pressed. Shutting down" % pin)
